ffaddb8341891cb04e5c9ae0b4c244356568e193
Br0k3nDesign <adam.j.moran@gmail.com>
2023-04-26 11:00:19 -0700

    Corrected URL

--- gss/main.py
+++ gss/main.py
@@ -8,10 +8,10 @@ def download():
     from tqdm import tqdm
     import requests
 
-    url = "http://download.thinkbroadband.com/10MB.zip"
+    url = settings.gss.url
     response = requests.get(url, stream=True)
 
-    with open("10MB", "wb") as handle:
+    with open(settings.gss.raw, "wb") as handle:
         for data in tqdm(response.iter_content()):
             handle.write(data)
 
